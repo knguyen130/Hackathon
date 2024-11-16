2135c72d3509a63d599b6e20c251574e0e66044e
programstarter <programstarter@gmail.com>
2024-11-16 12:52:14 -0800

    Make it do something

--- demo.py
+++ demo.py
@@ -3,7 +3,14 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def page1():
+    return "HELLO!"
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', person=name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=25565)
